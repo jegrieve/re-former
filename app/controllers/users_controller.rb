a26e4b210319979639ffe32176d0c0b650d10258
James Grieve <pocojames@hotmail.com>
2020-09-25 14:12:13 -0700

    add edit user form and error messages attached to failed model object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def new
+      @user = User.new
     end
     
     def create
@@ -12,6 +13,20 @@ class UsersController < ApplicationController
         end
       end
 
+      def edit
+        @user = User.find(params[:id])
+      end
+
+      def update
+        @user = User.find(params[:id])
+    
+        if @user.update(user_params)
+          redirect_to edit_user_path(@user)
+        else
+          render :edit
+        end
+      end
+      
       private
       def user_params
         params.require(:user).permit(:username,:password,:email)
